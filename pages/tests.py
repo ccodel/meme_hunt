fd06e9644dfda2338f34b645ec95b5555a640601
Cayden Codel <ccodel@andrew.cmu.edu>
2020-08-10 00:04:08 -0500

    WIP commit for adding tests on home and about page

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import SimpleTestCase, TestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from .views import HomePageView, AboutPageView
+from meme_hunt.lib.time import TimeConverter
+from meme_hunt.lib.test import standard_page_test
+
+class HomePageTests(TestCase):
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+        self.meme = Meme.objects.create(
+            subtitle = 'dummy meme',
+            start_date = TimeConverter.now(),
+            end_date = TimeConverter.now(),
+            secret_key = 12345
+        )
+        self.meme.save()
+
+    def test_standard(self):
+        standard_page_test(self, '/', 'home.html', HomePageView)
+
+    # def test_homepage_latest_meme(self):
+    #     now = TimeConverter.now()
+
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_standard(self):
+        standard_page_test(self, '/about/', 'about.html', AboutPageView)
